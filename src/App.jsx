7856775493cf3cd6ebc260cb90bf0d667fd78b8c
Aung Htet Oo <aunghtetoodawei11@gmail.com>
2024-07-03 19:47:59 +0630

    sh

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
    <>
     <div className="container">
     <img className="moon-image" src={moonImage} alt="Full Moon" />
-    <h1 className="moon-text">The Moon is in fact Full Moon on February 12nd of 2006 !!!!</h1>
+    <h1 className="moon-text">It is in fact Full Moon on February 12nd of 2006 !!!!</h1>
   </div>
    </>
   )
